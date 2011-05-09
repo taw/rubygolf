db656b7daf897e6c8369e891c802c8970091d33e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2011-05-09 23:05:59 +0100

    668 character solution

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,17 +1,17 @@
 class Golf
   class <<self
     def hole1 a
-      a.inject(&:*)
+      a.inject :*
     end
     def hole2 a
-      a.split.sort_by{|x|x[1]}.join " "
+      a.split.sort_by{|x|x[1]}*" "
     end
     def hole3 n
-      (1..n).inject(&:*)
+      hole1 1..n
     end
     def hole4 n
       n.map{|u|
-        u.gsub("cat","dead").gsub(/man(.*)/,'hat(man\1)').gsub /dog(.*)\)/,'dog\1(bone))'
+        u.gsub("cat","dead").gsub(/man.*/,'hat(\0)').gsub /dog(.*)\)/,'dog\1(bone))'
       }
     end
     def hole5 a
@@ -26,38 +26,37 @@ class Golf
     end
     def hole6 n
       (1..n).map{|n|
-        r = ""
-        r << "fizz" if n%3 == 0
-        r << "buzz" if n%5 == 0
-        r == "" ? n : r
+        r = ["fizz"][n%3]
+        r = "#{r}buzz" if n%5 == 0
+        r || n
       }
     end
     def hole7 r
       o=[]
       while a=b=r.shift
         b=r.shift while r[0]==b+1
-        o<<"#{a}#{a==b ?'':-b}"
+        o<<"#{a}#{a<b ?-b:''}"
       end
       o
     end
     def hole8 n
-      r=[0,1]
-      n.times{r<<r[-1]+r[-2]}
-      r[1..-2]
+      r=[1,1]
+      (n-2).times{r<<r[-1]+r[-2]}
+      r
     end
     def hole9 f
       v=File.readlines(f).map{|x|x.scan /\w+/}
-      while c=v.flatten.uniq
+      loop{
+        c=v.flatten.uniq
         e=0
         r=c.map{|a|
-          t=v.count{|j,|j==a}
-          e+=t
+          e+=t=v.count{|j,|j==a}
           [t,a]
         }.sort
         n,q=r[-1]
         break q if 2*n>=e
         v.map{|w|w.delete r[0][1]}
-      end
+      }
     end
   end
 end
